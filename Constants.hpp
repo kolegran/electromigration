f5f68471720f7dd71eb9caaba4dcad3e91844e98
kolegran <kolegran@gmail.com>
2018-06-07 13:02:45 +0300

    Added list of constants

---
+++ Constants.hpp
@@ -0,0 +1,32 @@
+#ifndef CONSTANTS_HPP
+#define CONSTANTS_HPP
+
+// Structure dimensions
+#define  NX         30
+#define  NY         30
+#define  NZ         1
+
+#define  Z          12                    // The number of coordination sphere
+
+#define  E_V        1.60217656535e-19     // Electronvolt constant
+#define  K_B        1.38064852e-23        // Boltzmann constant
+#define  NYU_0      1.0e+13               // Debye frequency
+#define  E_SADDLE   0                     // The saddle point energy
+#define  A_0        0.1                   // The initial artificial noise
+#define  DT         1.0e-14               // Time step
+#define  C_INIT     0.5                   // Initial distribution
+
+#define  Z_A       -15                    // The effective charge (from -10 to -30)
+#define  E_X        1.0e+8                // Projection of field strength on the axis X
+
+#define  E_CHARGE   1.60217656535e-19     // The electron charge
+#define  A_SPACING  2.5e-10               // The lattice (atomic) spacing (A----B)
+
+#define  T          550                   // The temperature
+
+#define  V_AA      -0.05*E_V              // The interaction energy
+#define  THETA      T*K_B
+
+#define  STEPS      100                   // Steps for writing a file
+
+#endif
